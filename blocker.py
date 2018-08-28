8f5fc5db8050cd51d102ca60633d45015ca97869
gauravgadekar <gauravgadekar2159@gmail.com>
2018-08-28 21:15:10 +0530

    First Commit

---
+++ blocker.py
@@ -0,0 +1,33 @@
+#/etc/hosts
+import time
+from datetime import datetime as dt
+hosts_temp="hosts"
+hosts_path="/etc/hosts"
+redirect="127.0.0.1"
+website_list=["www.facebook.com","facebook.com","www.youtube.com","youtube.com"]
+
+
+while True:
+    if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,20):
+        print("working hours")
+        with open(hosts_path,'r+') as file:
+            content=file.read()
+            print(content)
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect +" "+ website+ "\n")
+    else:
+
+
+        print("Fun Hours!")
+        with open(hosts_path,'r+') as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+                    file.truncate()
+
+    time.sleep(5)
